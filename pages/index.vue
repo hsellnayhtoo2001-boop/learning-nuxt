161142a602fda250e4b6ba756ae80e322a7f54cb
Arkar <arkarchanmyaeko@gmail.com>
2026-05-22 18:38:47 +0700

    create index and about pages

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+</script>
+
+<style></style>
+
+
+
+<!-- <template>
+  <div class="container">
+    <input
+      type="text"
+      placeholder="username"
+    >
+    <button class="btn-submit">Submit</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style>
+  .container {
+    /* background: green; */
+  }
+
+  .btn-submit {
+    background: red;
+    padding: 5px;
+  }
+</style> -->
